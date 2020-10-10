b1986e0dc1d11a48782cde30c9887d661c8af09e
hokrisztina <72090744+hokrisztina@users.noreply.github.com>
2020-10-10 19:52:58 +0200

    final

--- scripts.js
+++ scripts.js
@@ -1,45 +1,54 @@
-let data = [{
-    photo: 'firstpic.JPG',
-    title: 'My title',
-    description: 'What happened here, why is this a very nice image',
-  };
-  {
-    photo: '20191031_203433_074_IMG_0007.JPG',
-    title: 'Second picture',
-    description: 'Hahaha fun',
-  };
-  {
-  photo: '20191031_220226_624_IMG_0115.JPG',
+let imagesData = [{
+  photo: 'firstpic.JPG',
+  title: 'My title',
+  description: 'What happened here, why is this a very nice image',
+},
+{
+  photo: "secondpic.JPG",
+  title: 'Second picture',
+  description: 'Hahaha fun',
+},
+{
+  photo: 'thirdpic.JPG',
   title: 'Third picture',
   description: 'Another funny picture',
-};
+},
 {
-  photo: '20191031_214917_483_IMG_0105.JPG',
-    title: 'Fourth Picture',
-    description: 'Halloween funny pic',
-};
+  photo: 'fourthpic.JPG',
+  title: 'Fourth Picture',
+  description: 'Halloween funny pic',
+},
 {
-  photo: '20191031_220158_639_IMG_0113.JPG',
-    title: 'Last pic',
-    description: 'Haha really funny ninja',
-  };
-]
-$('#photo').attr('src', data.photo);
-let currentPhoto=0;
-let imagesData = [...] => {
+  photo: 'lastpic.JPG',
+  title: 'Last pic',
+  description: 'Haha really funny ninja',
+},
+];
+$('#photo').attr('src', imagesData.photo);
+let currentPhoto = 0;
+$('#photo').attr('src', imagesData[currentPhoto].photo);
+let loadPhoto = (photoNumber) => {
   $('#photo').attr('src', imagesData[currentPhoto].photo);
-}
-$('#cls-1').click(()=> {
-  if(currentPhoto>0){
-    currentPhoto--;
-}
-imagesData(currentPhoto);
-});
-$('#cls-2').click(()=> {
-  if(currentPhoto <5){
+$('#photo-title').text(imagesData[currentPhoto].title);
+$('#photo-description').text(imagesData[currentPhoto].description);
+};
+$('#rightArrow').click(() => {
+  if (currentPhoto < 5) {
     currentPhoto++;
-  }
-  imagesData(currentPhoto);
+  } else { currentPhoto = 0 };
+  loadPhoto(currentPhoto);
 });
-imagesData(currentPhoto);
-  
+$('#leftArrow').click(() => {
+  if (currentPhoto > 0) {
+    currentPhoto--;
+  } else { currentPhoto = 5 };
+  loadPhoto(currentPhoto);
+});
+imagesData.forEach((photo, index) => {
+  $('#thumbContainer').append(`<img class="thumb" data-index="${index}" src="${imagesData[index].photo}">`);
+  });
+$('.thumb').click((event) => {
+  let thumbClicked = $(event.target).attr('data-index');
+  currentPhoto = parseInt(thumbClicked);
+  loadPhoto(currentPhoto);
+});
